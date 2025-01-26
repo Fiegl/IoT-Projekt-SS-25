779aef28f39e6be77e2d1f4f1f109302d885acd8
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-01-26 15:47:34 +0100

    Update urls.py

--- iot_projekt/urls.py
+++ iot_projekt/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
+    path('', views.login_iot_view, name='login_iot'),
+    path('register_iot/', views.register_iot_view, name='register_iot'),
 ]
