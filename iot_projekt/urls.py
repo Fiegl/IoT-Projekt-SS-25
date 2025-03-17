59d169af6b2521cd6d7faa48168411529c3a2d09
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-03-17 20:03:59 +0100

    Delete iot_projekt directory

--- iot_projekt/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login_iot_view, name='login_iot'),
-    path('register_iot/', views.register_iot_view, name='register_iot'),
-]
