ecf2dab53beb4dfe2fa7604eea76ff5c3b916b1a
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-03-17 21:51:25 +0100

    Update views.py

--- iot_projekt/views.py
+++ iot_projekt/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.http import HttpResponse
+
 
 #Pfade zu den JSON-Dateien
 
 registrierte_benutzer = "/var/www/django-project/datenbank/users.json" 
 
+
+def home(request):
+    return HttpResponse("Willkommen auf der Startseite!")
