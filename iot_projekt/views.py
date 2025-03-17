7ac39880456b18a543357d8389e453350fd8b190
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-03-17 20:19:26 +0100

    Add files via upload

---
+++ iot_projekt/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.views import View
+
+#Pfade zu den JSON-Dateien
+
+registrierte_benutzer = "/var/www/django-project/datenbank/users.json" 
+
