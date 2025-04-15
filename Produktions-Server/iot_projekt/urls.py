e60937bf46c4eab0f64a09ff22b786b15bfcbcca
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-04-15 17:59:14 +0200

    Update urls.py

--- Produktions-Server/iot_projekt/urls.py
+++ Produktions-Server/iot_projekt/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.start, name='start'),
+    path('logout/', views.logout_view, name='logout'),
     path('registrieren/', views.registrieren, name='registrieren'),
     path('mainpage/', views.hauptseite, name='hauptseite'),
     path('passwort_vergessen/', views.passwort_vergessen, name='passwort_vergessen'),
