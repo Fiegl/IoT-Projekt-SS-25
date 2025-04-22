4dab1cabd83b668d5bbe84bd4b434bb13b81f003
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-04-22 17:01:50 +0200

    Update urls.py

--- Entwicklungs-Server/iot_projekt/urls.py
+++ Entwicklungs-Server/iot_projekt/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from .views import logout_view
 
 urlpatterns = [
     path('', views.start, name='start'),
     path('registrieren/', views.registrieren, name='registrieren'),
     path('mainpage/', views.hauptseite, name='hauptseite'),
-    path('logout/', logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout')
 ]
