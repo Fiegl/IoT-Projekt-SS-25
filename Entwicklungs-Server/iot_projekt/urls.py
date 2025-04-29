270f2ffaac5de5d41d61682a176fcc2a23170273
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-04-29 10:15:56 +0200

    Update urls.py

--- Entwicklungs-Server/iot_projekt/urls.py
+++ Entwicklungs-Server/iot_projekt/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.start, name='start'),
     path('registrieren/', views.registrieren, name='registrieren'),
     path('mainpage/', views.hauptseite, name='hauptseite'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    path('arbeitsplatz_buchen/', views.arbeitsplatz_buchen, name='arbeitsplatz_buchen'),
 ]
+
