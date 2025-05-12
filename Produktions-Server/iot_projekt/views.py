22e1e1cdfc9982052506a107d006128d7ead83b6
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-05-12 19:33:18 +0200

    Update views.py

--- Produktions-Server/iot_projekt/views.py
+++ Produktions-Server/iot_projekt/views.py
@@ -17,7 +17,6 @@ registrierte_benutzer = "/var/www/django-project/datenbank/users.json"
 reset_tokens = "/var/www/django-project/datenbank/reset_tokens.json"
 arbeitsplaetze = "/var/www/django-project/datenbank/arbeitsplaetze.json"
 
-
 #Hier ist die Funktion für den Decorator
 def login_required(view_func):
     @wraps(view_func)
@@ -36,16 +35,17 @@ def registrieren(request):
         email = request.POST.get("email")
         password = request.POST.get("passwort")
 
-        password_hash = make_password(password)
-
-
         with open(registrierte_benutzer, "r") as file:
             data = json.load(file)
 
+        if any(user["username"] == username for user in data["users"]):
+            return HttpResponse("Benutzername bereits vergeben.")
+
         new_user = {
+            "id": str(uuid.uuid4()),
             "username": username,
             "email": email,
-            "password": password_hash
+            "password": make_password(password)
         }
 
         data["users"].append(new_user)
@@ -56,6 +56,7 @@ def registrieren(request):
         return redirect("start")
 
     return render(request, 'iot_projekt/registrieren.html')
+
 
 @never_cache
 def start(request):
@@ -68,11 +69,16 @@ def start(request):
 
         for user in data["users"]:
             if user["username"] == username and check_password(password, user["password"]):
+      
                 request.session["username"] = username
+                request.session["user_id"] = user["id"]
+
                 return redirect("hauptseite")
-        return HttpResponse("Login fehlgeschlagen")
-    
+
+        return HttpResponse("Ihr Passwort oder Benutzername ist falsch, bitte erneut eingeben.")
+
     return render(request, 'iot_projekt/start.html')
+
 
 @never_cache
 @login_required
@@ -169,6 +175,70 @@ def passwort_zuruecksetzen(request, token):
 def hauptseite(request):
     if "username" not in request.session:
         return redirect("start")
-    return render(request, 'iot_projekt/mainpage.html')
+
+    with open(arbeitsplaetze, "r") as file:
+        arbeitsplaetze_data = json.load(file)["arbeitsplaetze"]
+        
+    user_id = request.session.get("user_id")
+
+    return render(request, 'iot_projekt/mainpage.html', {"arbeitsplaetze": arbeitsplaetze_data, "user_id": user_id})
+
+
+#Hier kommen die Funktionen für die An- und Abmeldung des Arbeitsplatzes
+
+@never_cache
+@login_required
+def arbeitsplatz_buchen(request):
+    if request.method == "POST":
+        desk_id = request.POST.get("desk_id")
+
+        with open(arbeitsplaetze, "r") as datei:
+            daten = json.load(datei)
+
+        
+        desk = None #Als Schutz vor unbefugten POST-Request (Manipulation von Formularen)
+        
+        for arbeitsplatz in daten["arbeitsplaetze"]:
+            if arbeitsplatz["id"] == desk_id:
+                desk = arbeitsplatz
+                break
+
+        if desk is not None:
+        
+            if desk["status"] == "frei":
+                desk["status"] = "belegt"
+                desk["user_id"] = request.session.get("user_id")
+
+                
+                with open(arbeitsplaetze, "w") as datei:
+                    json.dump(daten, datei, indent=4)
+
+    return redirect("hauptseite")
+
+
+@never_cache
+@login_required
+def arbeitsplatz_abmelden(request):
+    if "user_id" not in request.session:
+        return redirect("start")
+
+    user_id = request.session["user_id"]
+
+    with open(arbeitsplaetze, "r") as datei:
+        daten = json.load(datei)
+
+    for arbeitsplatz in daten["arbeitsplaetze"]:
+        if arbeitsplatz["user_id"] == user_id and arbeitsplatz["status"] == "belegt":
+            arbeitsplatz["status"] = "frei"
+            arbeitsplatz["user_id"] = None
+            break  
+
+    with open(arbeitsplaetze, "w") as datei:
+        json.dump(daten, datei, indent=4)
+
+    return redirect("hauptseite")
+
+
+
 
 
