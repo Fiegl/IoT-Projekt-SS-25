e37a0d98e9dde82fa66d1e4b0743bbdb68add5c9
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-06-02 12:34:46 +0200

    Update views.py

--- Entwicklungs-Server/iot_projekt/views.py
+++ Entwicklungs-Server/iot_projekt/views.py
@@ -3,7 +3,6 @@ import uuid
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password, check_password
-from django.core.mail import send_mail #wird f�r Passwort zur�cksetzen ben�tigt
 from functools import wraps #wird f�r den Decorator ben�tigt
 from django.views.decorators.cache import never_cache #verhindert den Cache
 from datetime import datetime
@@ -15,7 +14,6 @@ from .led_control import set_led_status
 # Pfad zu den JSON-Datenbanken
 
 registrierte_benutzer = "/var/www/django-project/datenbank/users.json"
-reset_tokens = "/var/www/django-project/datenbank/reset_tokens.json"
 arbeitsplaetze = "/var/www/django-project/datenbank/arbeitsplaetze.json"
 rechnungsbelege = "/var/www/django-project/datenbank/rechnungsbelege.json"
 
@@ -86,87 +84,6 @@ def logout_view(request):
     return redirect("start")
     
     
-#Hier sind die Funktionen f�r das Passwort zur�cksetzen (SMTP GMAIL, siehe settings.py)
-
-
-def load_reset_tokens():
-    try:
-        with open(reset_tokens, "r") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
-    except json.JSONDecodeError:
-        return {}
-
-def save_reset_tokens(tokens):
-    with open(reset_tokens, "w") as f:
-        json.dump(tokens, f, indent=4)
-
-
-def passwort_vergessen(request):
-    if request.method == "POST":
-        email = request.POST.get("email")
-
-        with open(registrierte_benutzer, "r") as file:
-            data = json.load(file)
-
-        for user in data["users"]:
-            if user["email"] == email:
-                token = str(uuid.uuid4())
-                tokens = load_reset_tokens()
-                tokens[token] = user["username"]
-                save_reset_tokens(tokens)
-
-                reset_link = request.build_absolute_uri(f"/passwort_zuruecksetzen/{token}/")
-
-                send_mail(
-                    'Passwort zur�cksetzen � Desk-Share-Lock',
-                    f'Hallo {user["username"]},\n\nHier ist dein Link:\n{reset_link}',
-                    'deinemail@gmail.com',
-                    [email],
-                    fail_silently=False,
-                )
-
-                return HttpResponse("E-Mail wurde versendet.")
-        
-        return HttpResponse("E-Mail nicht gefunden.")
-
-    return render(request, 'iot_projekt/passwort_vergessen.html')
-    
-    
-    
-    
-def passwort_zuruecksetzen(request, token):
-    tokens = load_reset_tokens()
-    username = tokens.get(token)
-
-    if not username:
-        return HttpResponse("Ung�ltiger oder abgelaufener Link.")
-
-    if request.method == "POST":
-        neues_passwort = request.POST.get("passwort")
-        neues_hash = make_password(neues_passwort)
-
-        with open(registrierte_benutzer, "r") as file:
-            data = json.load(file)
-
-        for user in data["users"]:
-            if user["username"] == username:
-                user["password"] = neues_hash
-                break
-
-        with open(registrierte_benutzer, "w") as file:
-            json.dump(data, file, indent=4)
-
-        # Token l�schen
-        del tokens[token]
-        save_reset_tokens(tokens)
-
-        return HttpResponse("Passwort erfolgreich ge�ndert!")
-
-    return render(request, 'iot_projekt/passwort_zuruecksetzen.html', {"token": token})
-
-
 
 #Hier die Funktion f�r die MainPage
 @never_cache
