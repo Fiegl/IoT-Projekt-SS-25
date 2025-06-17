8714bc508ce73fc09654438373a395c9e1ef35e7
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-06-17 11:32:32 +0200

    Update urls.py

--- Entwicklungs-Server/iot_projekt/urls.py
+++ Entwicklungs-Server/iot_projekt/urls.py
@@ -3,13 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.start, name='start'),
+    path('logout/', views.logout_view, name='logout'),
     path('registrieren/', views.registrieren, name='registrieren'),
     path('mainpage/', views.hauptseite, name='hauptseite'),
-    path('logout/', views.logout_view, name='logout'),
+    path('passwort_vergessen/', views.passwort_vergessen, name='passwort_vergessen'),
+    path('passwort_zuruecksetzen/<str:token>/', views.passwort_zuruecksetzen, name='passwort_zuruecksetzen'),
     path('arbeitsplatz_buchen/', views.arbeitsplatz_buchen, name='arbeitsplatz_buchen'),
     path('arbeitsplatz_abmelden/', views.arbeitsplatz_abmelden, name='arbeitsplatz_abmelden'),
     path('buchungsuebersicht', views.buchungsuebersicht, name='buchungsuebersicht'),
-    path('passwortaendern', views.passwortaendern, name='passwortaendern'),
+    path('passwort_aendern', views.passwort_aendern, name='passwort_aendern'),
+    path("profil_loeschen/", views.profil_loeschen, name="profil_loeschen"),
+    path('download_als_csv/', views.download_als_csv, name='download_als_csv'),
+    path("luxwert/", views.luxwert_aktuell, name="luxwert"),
 ]
 
 
